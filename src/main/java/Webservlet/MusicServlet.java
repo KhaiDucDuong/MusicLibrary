61cd317104d6c2991ed340c694832d23795604a5
ngodinhhuy-21110772 <ngodinhhuy38@gmail.com>
2024-05-09 00:57:57 +0700

    update try catch for all functions

--- src/main/java/Webservlet/MusicServlet.java
+++ src/main/java/Webservlet/MusicServlet.java
@@ -62,48 +62,58 @@ public class MusicServlet extends HttpServlet {
         List<Playlist> userPlaylists = PlaylistDB.selectPlaylist(user);
         request.setAttribute("userPlaylists", userPlaylists);
         //upload a song, author is set to current logged in user
-        if (action != null) {
-            if (action.equals("createMusic")) {
+        try {
+            if (action != null) {
+                if (action.equals("createMusic")) {
 
-                javax.servlet.http.HttpSession session = request.getSession();
+                    javax.servlet.http.HttpSession session = request.getSession();
 
-                if (session.getAttribute("insertMusicflag") == null) {
+                    if (session.getAttribute("insertMusicflag") == null) {
 
+                        try {
+                            message = createMusic(request, response, user);
+
+                            userUploadedSongs = MusicDB.selectMusicbyUserID(user);
+                            request.setAttribute("userUploadedSongs", userUploadedSongs);
+                            //get user's playlists
+                            userPlaylists = PlaylistDB.selectPlaylist(user);
+                            request.setAttribute("userPlaylists", userPlaylists);
+                        } catch (Exception e) {
+                            message = "Failed to upload song!";
+                            System.out.println("Failed to upload song!");
+                        }
+                        //set insertFlag so user has to create a new form to upload a song
+                        session.setAttribute("insertMusicflag", true);
+                    } else {
+                        //doesn't reupload the song and return a warning message
+                        message = "Please fill a new form to upload song!";
+                    }
+
+                    //return back to profile.jsp after uploading a song (new song is displayed there)
+                    //should change this to a playlist url that is dedicated for uploaded songs
+                    url = "/profile.jsp";
+                    request.setAttribute("message", message);
+                } else if (action.equals("Delete song")) {
                     try {
-                        message = createMusic(request, response, user);
-
+                        Long deletingSongID = Long.parseLong(request.getParameter("deletingSongID"));
+                        MusicDB.setMusicExistenceFalse(deletingSongID);
+                        url = "/profile.jsp";
                         userUploadedSongs = MusicDB.selectMusicbyUserID(user);
                         request.setAttribute("userUploadedSongs", userUploadedSongs);
                         //get user's playlists
                         userPlaylists = PlaylistDB.selectPlaylist(user);
                         request.setAttribute("userPlaylists", userPlaylists);
-                    } catch (Exception e) {
-                        message = "Failed to upload song!";
-                        System.out.println("Failed to upload song!");
+                        message = "Deleted song successfully!";
+                        request.setAttribute("message", message);
+                    } catch (Exception ex) {
+                        message = "Failed to delete song!";
+                        System.out.println("Failed to delete song!");
+                        ex.printStackTrace();
                     }
-                    //set insertFlag so user has to create a new form to upload a song
-                    session.setAttribute("insertMusicflag", true);
-                } else {
-                    //doesn't reupload the song and return a warning message
-                    message = "Please fill a new form to upload song!";
                 }
-
-                //return back to profile.jsp after uploading a song (new song is displayed there)
-                //should change this to a playlist url that is dedicated for uploaded songs
-                url = "/profile.jsp";
-                request.setAttribute("message", message);
-            } else if (action.equals("Delete song")) {
-                Long deletingSongID = Long.parseLong(request.getParameter("deletingSongID"));
-                MusicDB.setMusicExistenceFalse(deletingSongID);
-                url = "/profile.jsp";
-                userUploadedSongs = MusicDB.selectMusicbyUserID(user);
-                request.setAttribute("userUploadedSongs", userUploadedSongs);
-                //get user's playlists
-                userPlaylists = PlaylistDB.selectPlaylist(user);
-                request.setAttribute("userPlaylists", userPlaylists);
-                message = "Deleted song successfully!";
-                request.setAttribute("message", message);
             }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
         getServletContext()
@@ -117,79 +127,84 @@ public class MusicServlet extends HttpServlet {
     }
 
     private String createMusic(HttpServletRequest request, HttpServletResponse response, User author) {
-        String name = Encode.forHtml(request.getParameter("musicName"));
-        if (name.isEmpty()) {
-            return "Song name can't be empty!";
-        }
+        try {
+            String name = Encode.forHtml(request.getParameter("musicName"));
+            if (name.isEmpty()) {
+                return "Song name can't be empty!";
+            }
 
-        String category = request.getParameter("musicCategory");
-        int liked = 0;
-        int listen = 0;
+            String category = request.getParameter("musicCategory");
+            int liked = 0;
+            int listen = 0;
 //        String placeholder = request.getParameter("musicAuthor");
 //        long authorID = Long.parseLong(placeholder);
-        long millis = System.currentTimeMillis();
-        java.sql.Date date = new java.sql.Date(millis);
+            long millis = System.currentTimeMillis();
+            java.sql.Date date = new java.sql.Date(millis);
 
-        String imgPath = "images/songs_img/default-song.png";
+            String imgPath = "images/songs_img/default-song.png";
 
-        Music music = new Music(name, author, category, liked, listen, imgPath, date);
-        if (MusicDB.insertMusic(music)) {
-            //get song file
-            //if song file is not in the correct format, return error message
-            //and delete the inserted music in database
-            try {
-                Part songFile = request.getPart("musicFile");
-                String type = songFile.getContentType();
-                //mpeg is mp3
-                if (type != null) {
-                    String rename;
-                    if (type.equals("audio/mpeg")) {
-                        rename = "song" + music.getMusicID() + ".mp3";
+            Music music = new Music(name, author, category, liked, listen, imgPath, date);
+            if (MusicDB.insertMusic(music)) {
+                //get song file
+                //if song file is not in the correct format, return error message
+                //and delete the inserted music in database
+                try {
+                    Part songFile = request.getPart("musicFile");
+                    String type = songFile.getContentType();
+                    //mpeg is mp3
+                    if (type != null) {
+                        String rename;
+                        if (type.equals("audio/mpeg")) {
+                            rename = "song" + music.getMusicID() + ".mp3";
+                        } else {
+                            MusicDB.deleteMusic(music.getMusicID());
+                            return "Song File is not in the correct format!";
+                        }
+
+                        String songPath = "songs/" + rename;
+                        String absolutePath = request.getServletContext().getRealPath(songPath);
+                        songFile.write(absolutePath);
                     } else {
                         MusicDB.deleteMusic(music.getMusicID());
-                        return "Song File is not in the correct format!";
+                        return "Song file is empty!";
                     }
-
-                    String songPath = "songs/" + rename;
-                    String absolutePath = request.getServletContext().getRealPath(songPath);
-                    songFile.write(absolutePath);
-                } else {
+                } catch (IOException | ServletException ex) {
                     MusicDB.deleteMusic(music.getMusicID());
-                    return "Song file is empty!";
+                    return "Failed to read Song File! Error: " + ex.toString();
                 }
-            } catch (IOException | ServletException ex) {
-                MusicDB.deleteMusic(music.getMusicID());
-                return "Failed to read Song File! Error: " + ex.toString();
-            }
 
-            //get image file
-            try {
+                //get image file
+                try {
 
-                Part imageFile = request.getPart("imageFile");
-                String type = imageFile.getContentType();
-                if (type != null && (type.equals("image/jpeg") || type.equals("image/png"))) {
-                    String rename = "song" + music.getMusicID() + ".jpg";
-                    imgPath = "images/songs_img/" + rename;
-                    String absolutePath = request.getServletContext().getRealPath(imgPath);
-                    imageFile.write(absolutePath);
-                } else {
-                    return "Song Uploaded but Image must be a JPG or PNG";
+                    Part imageFile = request.getPart("imageFile");
+                    String type = imageFile.getContentType();
+                    if (type != null && (type.equals("image/jpeg") || type.equals("image/png"))) {
+                        String rename = "song" + music.getMusicID() + ".jpg";
+                        imgPath = "images/songs_img/" + rename;
+                        String absolutePath = request.getServletContext().getRealPath(imgPath);
+                        imageFile.write(absolutePath);
+                    } else {
+                        return "Song Uploaded but Image must be a JPG or PNG";
+                    }
+                } catch (IOException | ServletException ex) {
+                    return "Song Uploaded but Failed to upload Song Image! Error: " + ex.toString();
                 }
-            } catch (IOException | ServletException ex) {
-                return "Song Uploaded but Failed to upload Song Image! Error: " + ex.toString();
-            }
 
-            music.setImage(imgPath);
-            MusicDB.updateMusic(music);
+                music.setImage(imgPath);
+                MusicDB.updateMusic(music);
 //            Playlist playlistID = new Playlist();
 //            playlistID.setPlaylistID(3);
 //            Set<Music> addedSongs = new HashSet<Music>();
 //            addedSongs.add(music);
 //            PlaylistDB.addSongsToPlaylist(playlistID, addedSongs);
-            return "Upload song succesfully!";
+                return "Upload song succesfully!";
+            }
+
+            return "Failed to upload song!";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "An error occurred while processing the request.";
         }
-
-        return "Failed to upload song!";
-
+    
     }
 }
