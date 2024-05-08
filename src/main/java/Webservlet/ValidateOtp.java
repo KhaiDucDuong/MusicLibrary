3157ca0042afd525b22ea6af0919d24c3a0b975e
KhaiDucDuong <greenjohnny135@gmail.com>
2024-05-09 06:44:10 +0700

    fix parameter Tampering security
    add try catch block lol

--- src/main/java/Webservlet/ValidateOtp.java
+++ src/main/java/Webservlet/ValidateOtp.java
@@ -18,38 +18,30 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/ValidateOtp")
 public class ValidateOtp extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		int value=Integer.parseInt(request.getParameter("otp"));
-		HttpSession session=request.getSession();
-		int otp=(int)session.getAttribute("otp");
-		
-		
-		
-		RequestDispatcher dispatcher=null;
-		
-		
-		if (value==otp) 
-		{
-			
-				request.setAttribute("email", request.getParameter("email"));
-				request.setAttribute("status", "success");
-			  dispatcher=request.getRequestDispatcher("newPassword.jsp");
-			dispatcher.forward(request, response);
-			
-		}
-		else
-		{
-			request.setAttribute("message","wrong otp");
-			
-		   dispatcher=request.getRequestDispatcher("EnterOtp.jsp");
-			dispatcher.forward(request, response);
-		
-		}
-		
-	}
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        RequestDispatcher dispatcher = null;
+        String reponseUrl = "newPassword.jsp";
+        int otp = (int) session.getAttribute("otp");
+        try {
+            int value = Integer.parseInt(request.getParameter("otp"));
+            if (value == otp) {
 
+                request.setAttribute("email", request.getParameter("email"));
+                request.setAttribute("status", "success");
+
+            } else {
+                request.setAttribute("message", "wrong otp");
+                reponseUrl = "EnterOtp.jsp";
+            }
+        } catch (Exception e) {
+            request.setAttribute("message", "wrong otp");
+            reponseUrl = "EnterOtp.jsp";
+        } finally {
+            dispatcher = request.getRequestDispatcher(reponseUrl);
+            dispatcher.forward(request, response);
+        }
+    }
 }
